68187023e1b63be6ee4bd213dc990e245d6484d2
jstzwj <1103870790@qq.com>
2019-11-13 14:47:15 +0800

    update to newest tokio

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
-extern crate futures;
+#![warn(rust_2018_idioms)]
+
+extern crate tokio;
 
 mod server;
 
+use futures::executor::block_on;
+
 
 fn main() -> std::io::Result<()> {
-    server::app_loop()
+    
+    let future = server::app_loop(); // Nothing is printed
+    block_on(future);
+    Ok(())
 }
--- src/server.rs
+++ src/server.rs
@@ -1,46 +1,58 @@
-extern crate tokio;
 
-use tokio::io;
-use tokio::net::{TcpListener, TcpStream};
+use std::error::Error;
+use std::io;
+use std::sync::Arc;
+use std::sync::Mutex;
+use std::net::SocketAddr;
+
 use tokio::prelude::*;
+use tokio::net::{TcpListener, TcpStream};
 
-pub fn app_loop() -> std::io::Result<()> {
+
+struct Shared {
+}
+
+impl Shared {
+    /// Create a new, empty, instance of `Shared`.
+    fn new() -> Self {
+        Shared {
+        }
+    }
+
+}
+
+pub async fn app_loop() -> Result<(), Box<dyn Error>> {
+    let state = Arc::new(Mutex::new(Shared::new()));
     let addr_str = "127.0.0.1:25565";
-    let addr = addr_str.parse().unwrap();
-    let listener = TcpListener::bind(&addr).unwrap();
+    let mut listener = TcpListener::bind(&addr_str).await?;
 
-    // Here we convert the `TcpListener` to a stream of incoming connections
-    // with the `incoming` method. We then define how to process each element in
-    // the stream with the `for_each` combinator method
-    let server = listener
-        .incoming()
-        .for_each(|stream| {
-            // TODO: Process stream
-            let mut buf: [u8; 1] = [0; 1];
-            tokio::io::read_exact(stream, buf)
-                .and_then(|(stream, buf)|{
-                    if buf[0] == 0x01 {
-                        println!("ping");
-                    }
-                    Ok(())
-                })
-        })
-        .map_err(|err| {
-            // Handle error by printing to STDOUT.
-            println!("accept error = {:?}", err);
+    loop {
+        // Asynchronously wait for an inbound TcpStream.
+        let (stream, addr) = listener.accept().await?;
+
+        // Clone a handle to the `Shared` state for the new connection.
+        let state = Arc::clone(&state);
+
+        // Spawn our handler to be run asynchronously.
+        tokio::spawn(async move {
+            if let Err(e) = process(state, stream, addr).await {
+                println!("an error occured; error = {:?}", e);
+            }
         });
+    }
 
     println!("server running on {:?}", addr_str);
 
-    // Start the server
-    //
-    // This does a few things:
-    //
-    // * Start the Tokio runtime
-    // * Spawns the `server` task onto the runtime.
-    // * Blocks the current thread until the runtime becomes idle, i.e. all
-    //   spawned tasks have completed.
-    tokio::run(server);
+    Ok(())
+}
+
+
+/// Process an individual chat client
+async fn process(
+    state: Arc<Mutex<Shared>>,
+    stream: TcpStream,
+    addr: SocketAddr,
+) -> Result<(), Box<dyn Error>> {
 
     Ok(())
 }
